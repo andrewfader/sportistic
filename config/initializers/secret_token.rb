c3b657123a3349f987b2ce9c2f3348b07c2ef80e
Andrew Fader <andrevan@gmail.com>
2013-06-14 20:55:45 -0400

    Initial rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportistic::Application.config.secret_key_base = '368ade1347196ee9791b3812c24f65e4f032071236c488e8dd7bb5522177074e0d904b061eb4536329d82b013fe94d88be1e8a186333e5aa8c8162a95e0d330d'
