fd8cde2c769c0506217bb299d1da1dac13033a0c
Andrew Fader <andrevan@gmail.com>
2013-06-24 15:58:25 -0400

    only show matching teams when logged in

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < InheritedResources::Base
   authorize_resource
   def index
-    @matching_teams = Team.matching(current_user)
+    @matching_teams = Team.matching(current_user) if current_user
     super
   end
 end
