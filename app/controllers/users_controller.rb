4b344c6caa137159f05f6030cb8f289d7b0c34ff
Andrew Fader <andrevan@gmail.com>
2013-06-24 00:02:55 -0400

    rejigger registration flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
-class UsersController < ApplicationController
+class UsersController < InheritedResources::Base
   before_filter :authenticate_user!
-  def show
-    @user = User.find(params[:id])
+  def edit
+    @user = User.find(current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Sportistic::Application.routes.draw do
   root 'home#index'
-  devise_for :users, path_prefix: "my"
-  resources :users, only: [:show]
+  devise_for :users, path_prefix: "my", controllers: { registrations: "registrations" }
+  resources :users, only: [:show, :edit]
   resources :teams, only: [:new, :create, :edit, :show, :index]
 end
