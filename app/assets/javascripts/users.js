146ae5a0a0dfa6051a26a2cc9ff12f3f33e53a12
Andrew Fader <andrevan@gmail.com>
2013-07-06 21:37:23 -0700

    freebase autocompleters for locations

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,4 +17,6 @@ function readyUp() {
   );
 
   $('input[data-autocomplete]').railsAutocomplete();
+  $('#user_city').suggest({key: 'AIzaSyBZ1M9CIeCufVkulo1zYFtJ-xZsYJD81sM', filter:'(all type:/location/citytown)', flyout: false});
+  $('#team_location').suggest({key: 'AIzaSyBZ1M9CIeCufVkulo1zYFtJ-xZsYJD81sM', filter:'(all type:/location/citytown)', flyout: false});
 }
