9737a74fcc9f8e5493d6ed46f697ab0a4d958ebf
Andrew Fader <andrevan@gmail.com>
2013-07-17 23:20:33 -0700

    buttonize all links

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -15,6 +15,7 @@ function readyUp() {
   $('.ui-date-picker').datetimepicker(
     { dateFormat: 'dd-M-yy' }
   );
+  $('a').button();
 
   $('input[data-autocomplete]').railsAutocomplete();
   $('#user_city').suggest({key: 'AIzaSyBZ1M9CIeCufVkulo1zYFtJ-xZsYJD81sM', filter:'(all type:/location/citytown)', flyout: false});
