1a7dddd213eeb9639805a7d3f5e6c18a307d8383
Andrew Fader <andrevan@gmail.com>
2013-08-28 00:53:49 -0400

    sport picker

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -21,4 +21,21 @@ function readyUp() {
   $('input[data-autocomplete]').railsAutocomplete();
   $('#user_city').suggest({key: 'AIzaSyBZ1M9CIeCufVkulo1zYFtJ-xZsYJD81sM', filter:'(all type:/location/citytown)', flyout: false});
   $('#team_location').suggest({key: 'AIzaSyBZ1M9CIeCufVkulo1zYFtJ-xZsYJD81sM', filter:'(all type:/location/citytown)', flyout: false});
+
+  $('.pick_sport').click(function(event) {
+    var img1 = $(this).find('img')
+    var src1 = img1.attr("src")
+    var selected = $(this).data("selected")
+    if(selected) {
+      src1 = (src1 + "").replace('_selected','')
+      img1.attr("src", src1);
+      $(this).data("selected", false);
+    }
+    else {
+      src1 = (src1 + "").replace('.png','_selected.png')
+      img1.attr("src", src1);
+      $(this).data("selected", true);
+    }
+    event.preventDefault();
+  });
 }
